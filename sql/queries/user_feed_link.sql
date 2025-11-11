8670cf124b75ee2befd3d57af0c126bd67f3b035
Meet Bhesaniya <105696370+bhemi28@users.noreply.github.com>
2025-11-11 15:00:35 +0530

    Add GetUserFeedFollows query and format AddUserFeedLink

--- sql/queries/user_feed_link.sql
+++ sql/queries/user_feed_link.sql
@@ -2,3 +2,10 @@
 INSERT INTO user_feed_link (user_id, feed_id, created_at, updated_at)
 VALUES ($1, $2, $3, $4)
 RETURNING *;
+
+
+-- name: GetUserFeedFollows :many
+SELECT ufl.feed_id, ufl.created_at , f.title, f.url
+FROM user_feed_link ufl
+LEFT JOIN feeds f ON ufl.feed_id = f.id
+WHERE ufl.user_id = $1;
